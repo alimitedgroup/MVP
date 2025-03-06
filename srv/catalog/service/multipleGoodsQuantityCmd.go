b87be2f5aba89ff06564f2208ba5cf5f4be8e8ce
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2025-03-06 11:53:02 +0100

    Aggiornato catalogService: aggiunta modifica quantità e relativo command

---
+++ srv/catalog/service/multipleGoodsQuantityCmd.go
@@ -0,0 +1,20 @@
+package service
+
+import "github.com/alimitedgroup/MVP/common/stream"
+
+type MultipleGoodsQuantityCmd struct {
+	warehouseID string
+	goods       []stream.StockUpdateGood
+}
+
+func NewMultipleGoodsQuantityCmd(warehouseID string, goods []stream.StockUpdateGood) *MultipleGoodsQuantityCmd {
+	return &MultipleGoodsQuantityCmd{warehouseID: warehouseID, goods: goods}
+}
+
+func (mgqc *MultipleGoodsQuantityCmd) GetGoods() []stream.StockUpdateGood {
+	return mgqc.goods
+}
+
+func (mgqc *MultipleGoodsQuantityCmd) GetWarehouseID() string {
+	return mgqc.warehouseID
+}
--- srv/catalog/service/service.go
+++ srv/catalog/service/service.go
@@ -10,3 +10,12 @@ type CatalogService struct {
 func NewCatalogService(AddOrChangeGoodDataPort IAddOrChangeGoodDataPort, SetGoodQuantity ISetGoodQuantityPort, GetGoodsQuantity IGetGoodsQuantityPort, GetGoodsInfo IGetGoodsQuantityPort) *CatalogService {
 	return &CatalogService{addOrChangeGoodDataPort: AddOrChangeGoodDataPort, setGoodQuantity: SetGoodQuantity, getGoodsQuantity: GetGoodsQuantity, getGoodsInfo: GetGoodsInfo}
 }
+
+func (cs *CatalogService) SetMultipleGoodsQuantity(cmd *MultipleGoodsQuantityCmd) {
+	warehouseID := cmd.GetWarehouseID()
+	goodsSlice := cmd.GetGoods()
+
+	for i := range goodsSlice {
+		cs.setGoodQuantity.SetGoodQuantity(NewSetGoodQuantityCmd(warehouseID, goodsSlice[i].GoodID, goodsSlice[i].Quantity))
+	}
+}
