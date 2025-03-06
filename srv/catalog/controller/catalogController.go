c6e8e0c9ca0d73d42aa3feea831cf916d43a97ac
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2025-03-06 19:05:26 +0100

    Sistemato costruttore di catalogController

--- srv/catalog/controller/catalogController.go
+++ srv/catalog/controller/catalogController.go
@@ -3,29 +3,31 @@ package controller
 import (
 	"context"
 
+	service_portIn "github.com/alimitedgroup/MVP/srv/catalog/service/portIn"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/jetstream"
 )
 
 type catalogController struct {
+	getGoodsInfoUseCase             service_portIn.IGetGoodsInfoUseCase
+	getGoodsQuantityUseCase         service_portIn.IGetGoodsQuantityUseCase
+	setMultipleGoodsQuantityUseCase service_portIn.ISetMultipleGoodsQuantityUseCase
+	updateGoodDataUseCase           service_portIn.IUpdateGoodDataUseCase
 }
 
-func NewCatalogController() *catalogController {
-	return &catalogController{}
+func NewCatalogController(getGoodsInfoUseCase service_portIn.IGetGoodsInfoUseCase, getGoodsQuantityUseCase service_portIn.IGetGoodsQuantityUseCase, setMultipleGoodsQuantityUseCase service_portIn.ISetMultipleGoodsQuantityUseCase, updateGoodDataUseCase service_portIn.IUpdateGoodDataUseCase) *catalogController {
+	return &catalogController{getGoodsInfoUseCase: getGoodsInfoUseCase, getGoodsQuantityUseCase: getGoodsQuantityUseCase, setMultipleGoodsQuantityUseCase: setMultipleGoodsQuantityUseCase, updateGoodDataUseCase: updateGoodDataUseCase}
 }
 
-func (cc *catalogController) getGoodRequest(ctx context.Context, msg *nats.Msg) error {
-	return nil
+func (cc *catalogController) getGoodRequest(ctx context.Context, msg *nats.Msg) {
+
 }
 
-func (cc *catalogController) getWarehouseRequest(ctx context.Context, msg *nats.Msg) error {
-	return nil
+func (cc *catalogController) getWarehouseRequest(ctx context.Context, msg *nats.Msg) {
 }
 
-func (cc *catalogController) setGoodDataRequest(ctx context.Context, msg jetstream.Msg) error {
-	return nil
+func (cc *catalogController) setGoodDataRequest(ctx context.Context, msg jetstream.Msg) {
 }
 
-func (cc *catalogController) setGoodQuantityRequest(ctx context.Context, msg jetstream.Msg) error {
-	return nil
+func (cc *catalogController) setGoodQuantityRequest(ctx context.Context, msg jetstream.Msg) {
 }
