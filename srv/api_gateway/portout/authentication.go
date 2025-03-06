58a68e14dfaf8940e96d56073b831982be02b850
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-03-06 10:16:12 +0100

    api-gateway: complete authentication testing

--- srv/api_gateway/portout/authentication.go
+++ srv/api_gateway/portout/authentication.go
@@ -2,15 +2,23 @@ package portout
 
 type UserRole int
 
-type UserToken string
-
 const (
-	LocalAdmin UserRole = iota
-	GlobalAdmin
-	Client
+	RoleLocalAdmin UserRole = iota
+	RoleGlobalAdmin
+	RoleClient
+	RoleNone
 )
 
+type UserToken string
+
+const TokenNone UserToken = ""
+
+// AuthenticationPortOut è una porta di output che si occupa di gestire l'autenticazione
 type AuthenticationPortOut interface {
+	// GetToken ritorna un token per l'utente fornito se le
+	// credenziali sono valide, e TokenNone altrimenti.
 	GetToken(username string) (UserToken, error)
+	// GetRole ritorna il ruolo di un utente, dato il suo token.
+	// Se il token non è valido, viene ritornato RoleNone
 	GetRole(token UserToken) (UserRole, error)
 }
