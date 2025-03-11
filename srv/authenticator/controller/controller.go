f9e05ce44654bb1913deaa8eb93859e62f4ea50f
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2025-03-11 14:02:19 +0100

    Proseguo configurazione stream per Auth

--- srv/authenticator/controller/controller.go
+++ srv/authenticator/controller/controller.go
@@ -1,1 +1,18 @@
 package controller
+
+import (
+	"context"
+
+	"github.com/nats-io/nats.go"
+)
+
+type authController struct {
+}
+
+func NewAuthController() *authController {
+	return &authController{}
+}
+
+func (ar *authController) NewTokenRequest(ctx context.Context, msg *nats.Msg) error {
+	return nil
+}
--- srv/authenticator/main.go
+++ srv/authenticator/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/alimitedgroup/MVP/common/lib"
+	"github.com/alimitedgroup/MVP/srv/authenticator/config"
+	"github.com/alimitedgroup/MVP/srv/authenticator/controller"
+	"go.uber.org/fx"
+)
+
+func Run(ctx context.Context, cr *controller.ControllerRouter) error {
+	//var err error
+	err := cr.Setup(ctx)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return nil
+}
+
+func RunLifeCycle(lc fx.Lifecycle, cr *controller.ControllerRouter) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			err := Run(ctx, cr)
+			return err
+		},
+		OnStop: func(ctx context.Context) error {
+			return nil
+		},
+	})
+}
+
+var Modules = fx.Options(
+	lib.Module,
+	controller.Module,
+
+	//metti tutti altri module
+)
+
+func main() {
+	ctx := context.Background()
+	config := config.LoadConfig()
+	app := fx.New(
+		Modules,
+		config,
+		fx.Invoke(RunLifeCycle),
+	)
+
+	err := app.Start(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err = app.Stop(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
