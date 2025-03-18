64908d684502d72c4613e36acd57516917aad8b3
Loris Libralato <lorislibra21@gmail.com>
2025-03-18 20:52:28 +0100

    warehouse persistence test

--- srv/warehouse/adapter/persistence/stockRepositoryImpl_test.go
+++ srv/warehouse/adapter/persistence/stockRepositoryImpl_test.go
@@ -25,6 +25,10 @@ func TestStockRepositoryImplReserveAndUnreserve(t *testing.T) {
 	require.False(t, repo.SetStock("1", 10))
 	require.Nil(t, repo.ReserveStock("1", "1", 5))
 	require.Nil(t, repo.UnReserveStock("1", 5))
+
+	reserv, err := repo.GetReservation("1")
+	require.Nil(t, err)
+	require.Equal(t, reserv.Goods["1"], int64(5))
 }
 
 func TestStockRepositoryImplReserveNotExistingGood(t *testing.T) {
@@ -32,4 +36,16 @@ func TestStockRepositoryImplReserveNotExistingGood(t *testing.T) {
 
 	require.NotNil(t, repo.ReserveStock("1", "1", 5))
 	require.NotNil(t, repo.UnReserveStock("1", 5))
+	require.Zero(t, repo.GetFreeStock("1"))
+	reserv, err := repo.GetReservation("1")
+	require.NotNil(t, err)
+	require.Nil(t, reserv.Goods)
+}
+
+func TestStockRepositoryImplUnreserveNotEnough(t *testing.T) {
+	repo := NewStockRepositoryImpl()
+
+	require.False(t, repo.SetStock("1", 10))
+	require.Nil(t, repo.ReserveStock("1", "1", 5))
+	require.NotNil(t, repo.UnReserveStock("1", 6))
 }
