b494142076ed98a6752a3646d0820d179b6118a2
Loris Libralato <lorislibra21@gmail.com>
2025-03-19 14:27:41 +0100

    fix test

--- srv/order/adapter/persistence/stockRepositoryImpl_test.go
+++ srv/order/adapter/persistence/stockRepositoryImpl_test.go
@@ -12,13 +12,13 @@ func TestStockRepositoryImpl(t *testing.T) {
 	_, err := repo.GetStock("1", "1")
 	require.Error(t, err)
 	require.Equal(t, ErrWarehouseNotFound, err)
-	require.Equal(t, repo.GetGlobalStock("1"), 0)
+	require.Equal(t, repo.GetGlobalStock("1"), int64(0))
 
 	require.False(t, repo.SetStock("1", "2", 1))
 	stock2, err := repo.GetStock("1", "2")
 	require.NoError(t, err)
-	require.Equal(t, stock2, 2)
-	require.Equal(t, repo.GetGlobalStock("2"), 1)
+	require.Equal(t, stock2, int64(1))
+	require.Equal(t, repo.GetGlobalStock("2"), int64(1))
 
 	exist, err := repo.AddStock("1", "2", 1)
 	require.NoError(t, err)
