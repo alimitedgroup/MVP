7ba518b262a10ff4273c022272c3f3d0802c9da7
Loris Libralato <lorislibra21@gmail.com>
2025-03-19 14:55:40 +0100

    adapter test

--- srv/order/adapter/persistence/stockRepositoryImpl_test.go
+++ srv/order/adapter/persistence/stockRepositoryImpl_test.go
@@ -11,8 +11,13 @@ func TestStockRepositoryImpl(t *testing.T) {
 
 	_, err := repo.GetStock("1", "1")
 	require.Error(t, err)
-	require.Equal(t, ErrWarehouseNotFound, err)
+	require.Equal(t, err, ErrWarehouseNotFound)
 	require.Equal(t, repo.GetGlobalStock("1"), int64(0))
+
+	exist, err := repo.AddStock("1", "1", 1)
+	require.Error(t, err)
+	require.Error(t, err, ErrWarehouseNotFound)
+	require.False(t, exist)
 
 	require.False(t, repo.SetStock("1", "2", 1))
 	stock2, err := repo.GetStock("1", "2")
@@ -20,7 +25,17 @@ func TestStockRepositoryImpl(t *testing.T) {
 	require.Equal(t, stock2, int64(1))
 	require.Equal(t, repo.GetGlobalStock("2"), int64(1))
 
-	exist, err := repo.AddStock("1", "2", 1)
+	_, err = repo.GetStock("1", "3")
+	require.Error(t, err)
+
+	require.True(t, repo.SetStock("1", "2", 1))
+
+	exist, err = repo.AddStock("1", "3", 1)
+	require.Error(t, err)
+	require.Error(t, err, ErrGoodNotFound)
+	require.False(t, exist)
+
+	exist, err = repo.AddStock("1", "2", 1)
 	require.NoError(t, err)
 	require.True(t, exist)
 
