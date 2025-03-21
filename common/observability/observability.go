5bdf3398afa13860ae462e8176b585208a2f82b6
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-03-21 04:45:19 +0100

    observability.go

---
+++ common/observability/observability.go
@@ -0,0 +1,167 @@
+package observability
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/thessem/zap-prettyconsole"
+	"go.opentelemetry.io/contrib/bridges/otelzap"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"regexp"
+	"runtime/debug"
+	"strings"
+)
+
+func getBuildInfo() (string, string) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("ReadBuildInfo failed")
+		return "unknown", "unknown"
+	}
+
+	name := bi.Path
+
+	version := "unknown"
+
+	gitHashRegex := regexp.MustCompile(`^[a-f0-9]{40}$`)
+
+	dirty := false
+	for _, kv := range bi.Settings {
+		switch kv.Key {
+		case "vcs.revision":
+			version = kv.Value
+			if gitHashRegex.MatchString(version) {
+				version = version[:7]
+			}
+		case "vcs.modified":
+			dirty = kv.Value == "true"
+		}
+	}
+	if dirty {
+		version = version + "-dirty"
+	}
+
+	return name, version
+}
+
+// getLogLevel ritorna il livello massimo di log che verranno stampati.
+// Se la variabile d'ambiente `LOG_LEVEL` è presente, e ha
+// valori "error", "warn", "info" e "debug", allora questa
+// funzione ritorna il livello corrispondente.
+// Se la variabile d'ambiente non è definita,
+// allora viene ritornato il livello di default "debug".
+func getLogLevel() zapcore.Level {
+	env, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		return zap.DebugLevel
+	}
+
+	switch strings.ToLower(env) {
+	case "error":
+		return zap.ErrorLevel
+	case "warn":
+		return zap.WarnLevel
+	case "info":
+		return zap.InfoLevel
+	case "debug":
+		return zap.DebugLevel
+	default:
+		panic(fmt.Sprintf("invalid LOG_LEVEL: %s", env))
+	}
+}
+
+func setupZap(level zapcore.Level) *zap.Logger {
+	otel := otelzap.NewCore("core_name")
+
+	stderr := zapcore.NewCore(
+		prettyconsole.NewEncoder(prettyconsole.NewEncoderConfig()),
+		zapcore.Lock(os.Stderr),
+		level,
+	)
+
+	return zap.New(zapcore.NewTee(stderr, otel))
+}
+
+func setupOtel(otlpUrl string, tempLogger *zap.Logger) func(context.Context) error {
+	ctx := context.Background()
+
+	// Setup otel semantic conventions
+	name, version := getBuildInfo()
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(name),
+			semconv.ServiceVersion(version),
+			semconv.ServiceInstanceID(uuid.New().String()),
+		),
+	)
+	if err != nil {
+		tempLogger.Fatal("Failed to create OpenTelemetry resource", zap.Error(err))
+	}
+
+	// gRPC connection to the collector
+	conn, err := grpc.NewClient(
+		otlpUrl,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		tempLogger.Fatal(
+			"Failed to connect to OTLP endpoint",
+			zap.String("endpoint", otlpUrl),
+			zap.Error(err),
+		)
+	}
+
+	// Log exporter
+	exp, err := otlploggrpc.New(ctx, otlploggrpc.WithGRPCConn(conn))
+	if err != nil {
+		tempLogger.Fatal("Failed to create log exporter", zap.Error(err))
+	}
+	processor := log.NewBatchProcessor(exp)
+	logProvider := log.NewLoggerProvider(
+		log.WithResource(res),
+		log.WithProcessor(processor),
+	)
+
+	return func(ctx context.Context) error {
+		return logProvider.Shutdown(ctx)
+	}
+}
+
+// WrapLogger decora un logger con il nome specificato
+// Esempio d'uso:
+//
+//	fx.Decorate(WrapLogger("fancymodule"))
+func WrapLogger(name string) func(*zap.Logger) *zap.Logger {
+	return func(logger *zap.Logger) *zap.Logger {
+		return logger.Named(name)
+	}
+}
+
+func New(lc fx.Lifecycle) *zap.Logger {
+	level := getLogLevel()
+	tempLogger := prettyconsole.NewLogger(level)
+
+	otlpUrl := "localhost:4317"
+	otelcancel := setupOtel(otlpUrl, tempLogger)
+	logger := setupZap(level)
+
+	lc.Append(fx.Hook{
+		OnStop: otelcancel,
+	})
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return logger.Sync()
+		},
+	})
+
+	return logger
+}
