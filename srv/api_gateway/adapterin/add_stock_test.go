f7468a46fd721b47c1826e099d14824cfc404850
Loris Libralato <lorislibra21@gmail.com>
2025-03-27 11:05:02 +0100

    handle other errors

--- srv/api_gateway/adapterin/add_stock_test.go
+++ srv/api_gateway/adapterin/add_stock_test.go
@@ -3,9 +3,11 @@ package adapterin
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/alimitedgroup/MVP/common/dto/response"
 	"github.com/stretchr/testify/require"
 	gomock "go.uber.org/mock/gomock"
 )
@@ -28,4 +30,44 @@ func TestAddStock(t *testing.T) {
 	err = json.NewDecoder(resp.Body).Decode(&respbody)
 	require.NoError(t, err)
 	require.Empty(t, respbody)
+}
+
+func TestAddStockError(t *testing.T) {
+	s := start(t)
+	client := &http.Client{}
+
+	s.warehouses.EXPECT().AddStock(gomock.Any(), gomock.Any(), gomock.Any()).Return(fmt.Errorf("some error"))
+
+	payload := bytes.NewReader([]byte(`{"quantity": 10}`))
+
+	req, err := http.NewRequest(http.MethodPost, s.base+"/api/v1/goods/1/warehouse/1/add", payload)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+
+	var respbody response.ResponseDTO[string]
+	err = json.NewDecoder(resp.Body).Decode(&respbody)
+	require.NoError(t, err)
+	require.Equal(t, "internal_error", respbody.Error)
+}
+
+func TestAddStockMalformedInput(t *testing.T) {
+	s := start(t)
+	client := &http.Client{}
+
+	payload := bytes.NewReader([]byte(`"quantity": 10`))
+
+	req, err := http.NewRequest(http.MethodPost, s.base+"/api/v1/goods/1/warehouse/1/add", payload)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	var respbody response.ResponseDTO[string]
+	err = json.NewDecoder(resp.Body).Decode(&respbody)
+	require.NoError(t, err)
+	require.Equal(t, "internal_error", respbody.Error)
 }
--- srv/api_gateway/adapterin/update_good_test.go
+++ srv/api_gateway/adapterin/update_good_test.go
@@ -3,9 +3,11 @@ package adapterin
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/alimitedgroup/MVP/common/dto/response"
 	"github.com/stretchr/testify/require"
 	gomock "go.uber.org/mock/gomock"
 )
@@ -29,4 +31,44 @@ func TestUpdateGood(t *testing.T) {
 	err = json.NewDecoder(resp.Body).Decode(&respbody)
 	require.NoError(t, err)
 	require.Empty(t, respbody)
+}
+
+func TestUpdateGoodError(t *testing.T) {
+	s := start(t)
+	client := &http.Client{}
+
+	s.warehouses.EXPECT().UpdateGood(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(fmt.Errorf("some error"))
+
+	payload := bytes.NewReader([]byte(`{"name":"hat","description":"blue hat"}`))
+
+	req, err := http.NewRequest(http.MethodPut, s.base+"/api/v1/goods/1", payload)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+
+	var respbody response.ResponseDTO[string]
+	err = json.NewDecoder(resp.Body).Decode(&respbody)
+	require.NoError(t, err)
+	require.Equal(t, "internal_error", respbody.Error)
+}
+
+func TestUpdateGoodMalformedInput(t *testing.T) {
+	s := start(t)
+	client := &http.Client{}
+
+	payload := bytes.NewReader([]byte(`{"name":"hat","description""blue hat"`))
+
+	req, err := http.NewRequest(http.MethodPut, s.base+"/api/v1/goods/1", payload)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	var respbody response.ResponseDTO[string]
+	err = json.NewDecoder(resp.Body).Decode(&respbody)
+	require.NoError(t, err)
+	require.Equal(t, "internal_error", respbody.Error)
 }
