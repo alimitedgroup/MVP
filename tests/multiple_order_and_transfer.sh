f23f78d3a501d76bef369aca9e1a88e09d92526d
Loris Libralato <lorislibra21@gmail.com>
2025-03-27 11:22:20 +0100

    docs for api gateway and example test

---
+++ tests/multiple_order_and_transfer.sh
@@ -0,0 +1,39 @@
+curl -Ss -H "Content-Type: application/json" -X PUT localhost:8080/api/v1/goods/hat-1 \
+  -d '{"name":"hat","description":"blue hat"}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X PUT localhost:8080/api/v1/goods/hat-2 \
+  -d '{"name":"hat","description":"red hat"}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/goods/hat-1/warehouse/1/add \
+  -d '{"quantity": 10}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/goods/hat-1/warehouse/2/add \
+  -d '{"quantity": 7}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/goods/hat-2/warehouse/1/add \
+  -d '{"quantity": 5}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/goods/hat-2/warehouse/2/add \
+  -d '{"quantity": 5}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X GET localhost:8080/api/v1/goods | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/orders \
+  -d '{"name": "test-order-1", "full_name": "Mario Rossi", "address": "via roma 12 35012", "goods": {"hat-1": 13, "hat-2": 10}}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/orders \
+  -d '{"name": "test-order-2", "full_name": "Mario Rossi", "address": "via roma 12 35012", "goods": {"hat-1": 13, "hat-2": 10}}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/orders \
+  -d '{"name": "test-order-3", "full_name": "Mario Rossi", "address": "via roma 12 35012", "goods": {"hat-1": 13, "hat-2": 10}}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X GET localhost:8080/api/v1/orders | jq
+
+curl -Ss -H "Content-Type: application/json" -X GET localhost:8080/api/v1/goods | jq
+
+curl -Ss -H "Content-Type: application/json" -X POST localhost:8080/api/v1/transfers \
+  -d '{"receiver_id": "1", "sender_id": "2", "goods": {"hat-1": 2}}' | jq
+
+curl -Ss -H "Content-Type: application/json" -X GET localhost:8080/api/v1/transfers | jq
+
+curl -Ss -H "Content-Type: application/json" -X GET localhost:8080/api/v1/goods | jq
