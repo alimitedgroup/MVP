3313274e0798f2154ccfa37890e0c313c69b59bd
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-03-29 11:11:55 +0100

    refactor(notification): rename del package controller in adapterin

--- srv/notification/main.go
+++ srv/notification/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/alimitedgroup/MVP/common/lib"
+	"github.com/alimitedgroup/MVP/srv/notification/adapterin"
 	"github.com/alimitedgroup/MVP/srv/notification/adapterout"
-	"github.com/alimitedgroup/MVP/srv/notification/controller"
 	"github.com/alimitedgroup/MVP/srv/notification/persistence"
 	"github.com/alimitedgroup/MVP/srv/notification/service"
 	"go.uber.org/fx"
@@ -12,7 +12,7 @@ import (
 func main() {
 	app := fx.New(
 		lib.Module,
-		controller.Module,
+		adapterin.Module,
 		adapterout.Module,
 		service.Module,
 		persistence.Module,
